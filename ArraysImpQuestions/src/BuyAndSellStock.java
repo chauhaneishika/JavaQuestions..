7e02ce1fe6f900a78a3a572d1492fbf547f610a7
chauhaneishika <chauhaneishika@gmail.com>
2025-11-01 19:19:26 +0530

    Arrays important questions..

---
+++ ArraysImpQuestions/src/BuyAndSellStock.java
@@ -0,0 +1,21 @@
+public class BuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int buyAndSell = maxProfit(prices);
+        System.out.print(buyAndSell);
+
+    }
+
+}
