7ca06c57774810f7678f0ac873e760cd30db034d
chauhaneishika <chauhaneishika@gmail.com>
2025-11-02 20:47:38 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/ShallowAndDeepCopy.java
+++ Arrays2D.java/src/ShallowAndDeepCopy.java
@@ -1,2 +1,8 @@
 public class ShallowAndDeepCopy {
+    static void main() {
+        int[][] arr = {{6, 0, 2, 7, 6}, {1, 3, 7, 2, 2}, {9, 9, 4, 5, 2}};
+        int[][] brr = {{6, 0, 2, 7, 6}, {1, 3, 7, 2, 2}, {9, 9, 4, 5, 2}};
+        brr[1][3] = 20;
+        System.out.println(arr[1][3]);
+    }
 }
