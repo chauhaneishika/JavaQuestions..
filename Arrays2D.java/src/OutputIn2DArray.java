615f70244fd1e6c7d48df60855b89e057005dfde
chauhaneishika <chauhaneishika@gmail.com>
2025-11-02 21:02:03 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/OutputIn2DArray.java
+++ Arrays2D.java/src/OutputIn2DArray.java
@@ -13,26 +13,32 @@
 //    }
 //}
 
-import java.util.Scanner;
-
 public class OutputIn2DArray {
     static void main() {
-        Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[3][4];
-
-        System.out.println("Enter array elements: ");
-        for(int i = 0; i< arr.length; i++) {
-            for(int j = 0; j<arr[0].length; j++) {
-                arr[i][j] = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        int[][] arr = new int[3][4];
+//
+//        System.out.println("Enter array elements: ");
+//        for(int i = 0; i< arr.length; i++) {
+//            for(int j = 0; j<arr[0].length; j++) {
+//                arr[i][j] = sc.nextInt();
+//            }
+//            System.out.println();
+//        }
+//
+//        for(int i = 0; i<arr.length; i++) {
+//            for(int j = 0; j<arr[0].length; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        int[][] arr = {{6,0,2,7},{1,3,7,2},{9,9,4,5}};
+        int sum = 0;
+        for (int i =0; i<arr.length; i++) {
+            for(int j = 0; j<arr[0].length; j++){
+                sum += arr[i][j];
             }
-            System.out.println();
         }
-
-        for(int i = 0; i<arr.length; i++) {
-            for(int j = 0; j<arr[0].length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println(sum);
     }
 }
