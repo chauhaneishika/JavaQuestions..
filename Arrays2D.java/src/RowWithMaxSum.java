769f04fa4a66ac0d533aafcb4344f68c10371caa
chauhaneishika <chauhaneishika@gmail.com>
2025-11-03 21:28:42 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/RowWithMaxSum.java
+++ Arrays2D.java/src/RowWithMaxSum.java
@@ -2,14 +2,17 @@ public class RowWithMaxSum {
     static void main() {
         int[][] arr = {{6,0,2,7,6},{1,3,7,2,2},{9,9,4,5,2}};
         int maxSum = Integer.MIN_VALUE;
-        //int row = -1;
+        int row = -1;
         for(int i=0; i<arr.length; i++) {
             int sum = 0;
             for(int j=0; j<arr[0].length; j++) {
                 sum += arr[i][j];
             }
-            maxSum = Math.max(maxSum, sum);
+            if(sum > maxSum){
+                maxSum = sum;
+                row = i;
+            }
         }
-        System.out.println(maxSum);
+        System.out.println(row + " " + maxSum);
     }
 }
