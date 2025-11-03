c41ce0c45ecf44de03ee5686902b7bc5534b5711
chauhaneishika <chauhaneishika@gmail.com>
2025-11-03 22:12:56 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/ColumnWisePrint.java
+++ Arrays2D.java/src/ColumnWisePrint.java
@@ -1,24 +1,11 @@
 public class ColumnWisePrint {
     static void main() {
         int[][] arr = {{6,0,2,7,6},{1,3,7,20,2},{9,9,4,5,2}};
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i][0]+" ");
-        }
-        System.out.println();
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i][1]+" ");
-        }
-        System.out.println();
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i][2]+" ");
-        }
-        System.out.println();
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i][3]+" ");
-        }
-        System.out.println();
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i][4]+" ");
+        for(int j=0; j<arr[0].length; j++) {
+            for(int i=0; i<arr.length; i++ ) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
         }
 
     }
