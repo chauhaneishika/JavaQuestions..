c4a34975dbfbce82734d22ac095475822984236b
chauhaneishika <chauhaneishika@gmail.com>
2025-11-05 21:29:06 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/Transpose.java
+++ Arrays2D.java/src/Transpose.java
@@ -1,5 +1,17 @@
 public class Transpose {
     static void main() {
+        int[][] arr = {{2,8,3,4},{7,2,1,6},{5,5,4,1},{3,1,8,2}};
+        print(arr);
 
+    }
+
+    private static void print(int[][] arr) {
+        for(int[] a : arr){
+            for(int ele : a){
+                System.out.print(ele+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
