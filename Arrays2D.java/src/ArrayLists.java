2950f6ab2acae951bd664cab8fb6f718341f0119
chauhaneishika <chauhaneishika@gmail.com>
2025-11-17 20:07:36 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/ArrayLists.java
+++ Arrays2D.java/src/ArrayLists.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class ArrayLists {
+    static void main() {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(3); a.add(13); a.add(36); a.add(132);
+        ArrayList<Integer> b = new ArrayList<>();
+        a.add(1); a.add(2); a.add(3);
+        ArrayList<Integer> c = new ArrayList<>();
+        a.add(2); a.add(3); a.add(5); a.add(7); a.add(11);
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        arr.add(a); arr.add(b); arr.add(c);
+
+        System.out.println(arr);
+
+
+    }
 
 }
