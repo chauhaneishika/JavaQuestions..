2c3667fa636a6846848e130f48812afc133c1fd8
chauhaneishika <chauhaneishika@gmail.com>
2025-11-21 19:34:49 +0530

    Learning Multi-dimensional Arrays..

--- Arrays2D.java/src/Multiply2Matrices.java
+++ Arrays2D.java/src/Multiply2Matrices.java
@@ -1,3 +1,39 @@
-public class Multiply2Matrices {
+import java.util.ArrayList;
 
+public class Multiply2Matrices {
+    public static ArrayList<ArrayList<Integer>> multiply(int[][] a, int[][] b){
+        int n = a.length;
+        int[][] c = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    c[i][j] += (a[i][k]*b[k][j]);
+                }
+            }
+        }
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            ans.add(new ArrayList<>());
+            for(int j=0;j<n;j++){
+                ans.get(i).add(c[i][j]);
+            }
+        }
+        return ans;
+    }
+
+    static void main() {
+        int[][] a = {
+                {1,2,3},
+                {1,2,3},
+                {1,2,3}
+        };
+        int[][] b = {
+                {1,1,1},
+                {2,2,2},
+                {3,3,3}
+        };
+        ArrayList<ArrayList<Integer>> ans = Multiply2Matrices.multiply(a,b);
+        System.out.println(ans);
+
+    }
 }
