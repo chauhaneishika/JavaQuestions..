957005ac0b22d236d936e19d6795a776259d879c
chauhaneishika <chauhaneishika@gmail.com>
2025-11-26 20:30:57 +0530

    Learning Multi-dimensional Arrays..

--- ArraysRevision/src/BuyAndSellStockInput.java
+++ ArraysRevision/src/BuyAndSellStockInput.java
@@ -1,3 +1,35 @@
-public class BuyAndSellStockInput {
+import java.util.Scanner;
 
+public class BuyAndSellStockInput {
+    public static int maxProfit(int[] prices){
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++) {
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else if (prices[i] - minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter prices: ");
+        for(int i=0; i<n; i++){
+            prices[i] = sc.nextInt();
+        }
+
+        int result = maxProfit(prices);
+        System.out.println("Max Profit = "+ result);
+    }
 }
