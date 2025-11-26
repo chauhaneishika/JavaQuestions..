0fdddec429367f9d5434a67d09f4f3696f5404fd
chauhaneishika <chauhaneishika@gmail.com>
2025-11-26 20:57:27 +0530

    Learning Multi-dimensional Arrays..

--- ArraysRevision/src/MoveZeroesInput.java
+++ ArraysRevision/src/MoveZeroesInput.java
@@ -1,3 +1,36 @@
-public class MoveZeroesInput {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class MoveZeroesInput {
+    public static void moveZeores(int[] nums){
+        int insertPosition = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0) {
+                nums[insertPosition] = nums[i];
+                insertPosition++;
+            }
+        }
+        while(insertPosition< nums.length){
+            nums[insertPosition++] = 0;
+        }
+    }
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+
+        int[]nums = new int[n];
+
+        System.out.println("Enter elements: ");
+        for(int i=0; i<n; i++){
+            nums[i]= sc.nextInt();
+        }
+        moveZeores(nums);
+
+        System.out.println("Output array:");
+        System.out.print(Arrays.toString(nums));
+    }
 }
