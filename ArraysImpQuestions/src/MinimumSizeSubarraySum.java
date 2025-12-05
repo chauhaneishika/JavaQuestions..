61572b354d4eef3f147f7fbc297bd9fc3969f77a
chauhaneishika <chauhaneishika@gmail.com>
2025-12-05 22:25:22 +0530

    Learning Multi-dimensional Arrays..

--- ArraysImpQuestions/src/MinimumSizeSubarraySum.java
+++ ArraysImpQuestions/src/MinimumSizeSubarraySum.java
@@ -1,3 +1,23 @@
 public class MinimumSizeSubarraySum {
+    int minSubArrayLen(int[] nums, int target){
+        int minLenWindow = Integer.MAX_VALUE;
+        int currentSum = 0;
 
+        int low =0;
+        int high = 0;
+        while(high < nums.length){
+            currentSum += nums[high];
+            high++;
+
+            while(currentSum >= target) {
+                int currentWindowSize = high - low;
+
+                minLenWindow = Math.min(minLenWindow, currentWindowSize);
+
+                currentSum -= nums[low];
+                low++;
+            }
+        }
+        return minLenWindow == Integer.MAX_VALUE ? 0 : minLenWindow;
+    }
 }
