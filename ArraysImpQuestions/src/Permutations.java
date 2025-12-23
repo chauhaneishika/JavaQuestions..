17917485e5d0355d00d5da3eb2a676c518b075d5
chauhaneishika <chauhaneishika@gmail.com>
2025-12-23 22:59:39 +0530

    Learning Multi-dimensional Arrays..

---
+++ ArraysImpQuestions/src/Permutations.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtrack(resultList, new ArrayList<>(), nums);
+        return resultList;
+    }
+
+    private void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList,int[] nums) {
+        if(tempList.size() == nums.length ){
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+        for(int number : nums) {
+            if(tempList.contains(number))
+                continue;
+
+            tempList.add(number);
+
+            backtrack(resultList, tempList, nums);
+
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
