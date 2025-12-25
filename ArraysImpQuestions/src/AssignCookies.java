376852deccd0c5566f73404bfc1c50f1fef96199
chauhaneishika <chauhaneishika@gmail.com>
2025-12-25 23:09:22 +0530

    Learning Multi-dimensional Arrays..

---
+++ ArraysImpQuestions/src/AssignCookies.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int contentChildren = 0;
+        int cookieIndex = 0;
+
+        while(cookieIndex < s.length && contentChildren < g.length) {
+            if(s[cookieIndex] >= g[contentChildren] ) {
+                contentChildren++;
+            }
+            cookieIndex++;
+        }
+        return contentChildren;
+    }
+}
